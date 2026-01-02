917cba9b0df4d2ca36b66589c42ad6cf737b7828
Durg Singh <durg.singh@nagarro.com>
2026-01-02 13:35:46 +0530

    using ng-content

--- src/app/task/user-task/user-task.component.ts
+++ src/app/task/user-task/user-task.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Task } from './task.model';
+import { CardComponent } from "../../shared/card/card.component";
 
 @Component({
   selector: 'app-user-task',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user-task.component.html',
   styleUrl: './user-task.component.css'
 })
@@ -16,5 +17,5 @@ export class UserTaskComponent {
   onCompleteTask(){
     this.complete.emit(this.task.id);
   }
-  
+
 }
